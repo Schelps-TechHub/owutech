bb6420c45b45862a2192dc9bc18e7b8a36e5fbdc
schelpstech <sunkanmi@schelps.com.ng>
2021-11-04 10:42:30 +0100

    eee

--- student/pages/welcome.php
+++ student/pages/welcome.php
@@ -30,17 +30,29 @@ include "../include/header.php";
         </div>
         <div class="container-fluid">
             <div class="row clearfix">
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card widget_2 big_icon traffic">
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card widget_2 big_icon documents">
                         <div class="body">
-                            <h6>Registration<br> Forms</h6>
+                        <h6>Courses</h6>
+                            <h2>0 <small class="info">Registered</small></h2>
+                            <small>as at today <?php echo date("d-m-Y");?></small>
+                            <div class="progress">
+                                <div class="progress-bar l-green" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card widget_2 big_icon email">
+                        <div class="body">
+                            <h6>Examination</h6>
                             <h2> <?php
                             $sql = "SELECT count('id') FROM form_owner where userid = '$id'";
                             $result = mysqli_query($con,$sql);
                             $row = mysqli_fetch_array($result);
                            echo $row['0'];
                             ?>
-                                <small class="info"> Forms</small></h2>
+                                <small class="info"> Active</small></h2>
                             <small>as at today <?php echo date("d-m-Y");?></small>
                             <div class="progress">
                                 <div class="progress-bar l-amber" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
@@ -49,15 +61,15 @@ include "../include/header.php";
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card widget_2 big_icon email">
+                    <div class="card widget_2 big_icon sales">
                         <div class="body">
-                        <h6>Submitted<br> Forms</h6>
+                        <h6>Payment <br> </h6>
                             <h2><?php
                             $sql = "SELECT count('id') FROM form_owner where userid = '$id' and `status` = 1";
                             $result = mysqli_query($con,$sql);
                             $row = mysqli_fetch_array($result);
                            echo $row['0'];
-                            ?> <small class="info"> Forms</small></h2>
+                            ?> <small class="info"> Due Payments</small></h2>
                             <small>as at today <?php echo date("d-m-Y");?></small>
                             <div class="progress">
                                 <div class="progress-bar l-blue" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
@@ -66,15 +78,49 @@ include "../include/header.php";
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card widget_2 big_icon email">
+                    <div class="card widget_2 big_icon documents">
                         <div class="body">
-                        <h6>Unsubmitted<br> Forms</h6>
+                        <h6>Result </h6>
                             <h2><?php
                             $sql = "SELECT count('id') FROM form_owner where userid = '$id' and `status` = 0";
                             $result = mysqli_query($con,$sql);
                             $row = mysqli_fetch_array($result);
                            echo $row['0'];
-                            ?> <small class="info"> Forms</small></h2>
+                            ?> <small class="info"> Published</small></h2>
+                            <small>as at today <?php echo date("d-m-Y");?></small>
+                            <div class="progress">
+                                <div class="progress-bar l-amber" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+            </div>
+           
+            <div class="row clearfix">
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card widget_2 big_icon traffic">
+                        <div class="body">
+                        <h6>Tickets</h6>
+                            <h2>0 <small class="info">Support Tickets</small></h2>
+                            <small>as at today <?php echo date("d-m-Y");?></small>
+                            <div class="progress">
+                                <div class="progress-bar l-green" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card widget_2 big_icon storage">
+                        <div class="body">
+                            <h6>E-library</h6>
+                            <h2> <?php
+                            $sql = "SELECT count('id') FROM form_owner where userid = '$id'";
+                            $result = mysqli_query($con,$sql);
+                            $row = mysqli_fetch_array($result);
+                           echo $row['0'];
+                            ?>
+                                <small class="info"> Learning Material</small></h2>
                             <small>as at today <?php echo date("d-m-Y");?></small>
                             <div class="progress">
                                 <div class="progress-bar l-amber" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
@@ -85,18 +131,39 @@ include "../include/header.php";
                 <div class="col-lg-3 col-md-6 col-sm-12">
                     <div class="card widget_2 big_icon domains">
                         <div class="body">
-                        <h6>Admission <br> Letters</h6>
-                            <h2>0 <small class="info">applicants</small></h2>
+                        <h6>Student <br> Affairs </h6>
+                            <h2><?php
+                            $sql = "SELECT count('id') FROM form_owner where userid = '$id' and `status` = 1";
+                            $result = mysqli_query($con,$sql);
+                            $row = mysqli_fetch_array($result);
+                           echo $row['0'];
+                            ?> <small class="info"> Activities</small></h2>
                             <small>as at today <?php echo date("d-m-Y");?></small>
                             <div class="progress">
-                                <div class="progress-bar l-green" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+                                <div class="progress-bar l-blue" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card widget_2 big_icon traffic">
+                        <div class="body">
+                        <h6>Calendar </h6>
+                            <h2><?php
+                            $sql = "SELECT count('id') FROM form_owner where userid = '$id' and `status` = 0";
+                            $result = mysqli_query($con,$sql);
+                            $row = mysqli_fetch_array($result);
+                           echo $row['0'];
+                            ?> <small class="info"> Upcoming Events</small></h2>
+                            <small>as at today <?php echo date("d-m-Y");?></small>
+                            <div class="progress">
+                                <div class="progress-bar l-amber" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
             </div>
-           
-           
           
             
         </div>
